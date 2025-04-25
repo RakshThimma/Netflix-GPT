47b45e5bee97721aa73232859da31c04fca3b902
RakshThimma <rakshithathimma@gmail.com>
2025-04-25 12:54:04 +0530

    trailer video fix

--- src/components/VideoBackGround.js
+++ src/components/VideoBackGround.js
@@ -5,11 +5,12 @@ import { useSelector } from "react-redux";
 const VideoBackGround = ({ movie_id }) => {
   useGetMovieVideo(movie_id); //fetches the movie with the given movie_id and stores the trailer of movie in redux store
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo); //fetching details of trailer from store
+  if(!trailerVideo) {return <div>Loading...</div>}
 
   return (
     <div>
       <iframe
-        className="w-screen aspect-video "
+        className="w-screen aspect-video"
         src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&controls=0&rel=0&showinfo=0`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
